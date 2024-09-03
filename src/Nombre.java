648d0856dcb2dc1b42f5da8631b652f069d9660a
Joel <joel.gonzalez2401@alumnos.ubiobio.cl>
2024-09-03 19:15:39 -0400

    clases creadas

---
+++ src/Nombre.java
@@ -0,0 +1,59 @@
+import java.util.Objects;
+
+public class Nombre {
+
+    private Tratamiento tratamiento;
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+
+    public Tratamiento getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(Tratamiento tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre{" +
+                "tratamiento=" + tratamiento +
+                ", nombres='" + nombres + '\'' +
+                ", apellidoPaterno='" + apellidoPaterno + '\'' +
+                ", apellidoMaterno='" + apellidoMaterno + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Nombre nombre = (Nombre) o;
+        return Objects.equals(nombres, nombre.nombres) && Objects.equals(apellidoPaterno, nombre.apellidoPaterno) && Objects.equals(apellidoMaterno, nombre.apellidoMaterno);
+    }
+}
