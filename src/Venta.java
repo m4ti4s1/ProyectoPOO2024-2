571937c93d39586b741e1052553b02f24a23c15e
Vicho2036 <vicente.cifuentes2401@alumnos.ubiobio.cl>
2024-09-04 11:52:47 -0400

    Revisar clases para implementacion, falta las relaciones, agregacion y composicion

---
+++ src/Venta.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate;
+
+public class Venta {
+    private String idDocumento;
+    private TipoDocumento tipo;
+    private LocalDate fecha;
+
+    public Venta(String id, TipoDocumento tipo, LocalDate fec, Cliente cli) {
+        this.idDocumento = idDocumento;
+        this.tipo = tipo;
+        this.fecha = fec;
+        this.cliente = cli;
+
+    }
+
+    public String getIdDocumento() {
+        return idDocumento;
+    }
+
+    public TipoDocumento getTipo() {
+        return tipo;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public createPasaje(int asiento, Viaje viaje,Pasajero pasajero) {
+        Pasaje pasaje = new Pasaje(asiento,viaje,pasajero,this);
+    }
+    public String[] getPasajes(){
+        return Pasaje[];
+
+    }
+    public int getMonto(){
+        return monto;
+    }
+
+}
