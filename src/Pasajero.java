0fd3f8a4a2a81fe2c75498b9faeb409c6452b2da
m4ti4s1 <m4ti4s.404@gmail.com>
2024-09-05 12:00:13 -0400

    Arreglando errores e implementando Relacion entre clases Venta y Cliente

--- src/Pasajero.java
+++ src/Pasajero.java
@@ -1,7 +1,6 @@
 public class Pasajero extends Persona {
     private Nombre nomContacto;
-    private String fonoContancto;
-
+    private String fonoContacto;
 
     public Nombre getNomContacto() {
         return nomContacto;
@@ -12,10 +11,10 @@ public class Pasajero extends Persona {
     }
 
     public String getFonoContancto() {
-        return fonoContancto;
+        return fonoContacto;
     }
 
     public void setFonoContancto(String fono) {
-        this.fonoContancto = fono;
+        this.fonoContacto = fono;
     }
 }
