9f25c9a73ee37fb9423ec98e9504afb8be00b88d
m4ti4s1 <m4ti4s.404@gmail.com>
2024-09-07 19:38:02 -0400

    Comenzando Clase menu

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,56 @@ public class Main {
 
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Main main = new Main();
+        int opc = 0;
+
+        do {
+            main.menu();
+            opc = main.elegirOpc(8, sc);
+
+            switch(opc) {
+                case 1:
+                    main.createCliente();
+                    break;
+                case 2:
+                    main.createBus();
+                    break;
+                case 3:
+                    main.createViaje();
+                    break;
+                case 4:
+                    main.vendePasaje();
+                    break;
+                case 5:
+                    main.listPasajerosViaje();
+                    break;
+                case 6:
+                    main.listVentas();
+                    break;
+                case 7:
+                    main.listViajes();
+                    break;
+                case 8:
+                    System.out.println("Saliendo...");
+                    break;
+            }
+        } while (opc != 8);
     }
+
     private void menu() {
-        System.out.println();
+        System.out.println("============================");
+        System.out.println("...::: Menu principal :::...\n");
+        System.out.println("1) Crear cliente");
+        System.out.println("2) Crear bus");
+        System.out.println("3) Crear viaje");
+        System.out.println("4) Vender pasajes");
+        System.out.println("5) Lista de pasajero");
+        System.out.println("6) Lista de ventas");
+        System.out.println("7) Lista de viajes");
+        System.out.println("8) Salir");
+        System.out.println("----------------------------");
+        System.out.print("..:: Ingrese numero de opcion: ");
     }
 
     private void createCliente() {
@@ -36,5 +83,23 @@ public class Main {
 
     private void listViajes() {
 
+    }
+
+    private int elegirOpc(int cantOpciones, Scanner sc) {
+        int opc = 0;
+
+        boolean valido = false;
+        while (!valido) {
+            opc = sc.nextInt();
+
+            System.out.println();
+
+            if (opc > 0 && opc <= cantOpciones){
+                valido = true;
+            } else {
+                System.out.println("Opcion invalida, intente denuevo");
+            }
+        }
+        return opc;
     }
 }
